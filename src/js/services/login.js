449df464952e19dd0c577428d7c1791e09761284
quanxiaoxiao <quan_xiaoxiao@qq.com>
2015-07-02 11:17:50 +0800

    xxx

--- src/js/services/login.js
+++ src/js/services/login.js
@@ -4,29 +4,6 @@ var loginService = function($q, loginResource, callSocket) {
 	return {
 		login: function(data) {
 			var defer = $q.defer();
-			/*
-			return loginResource
-						.login(data)
-						.then(function(response) {
-							if (response.isSuccess) {
-								return 'ok';
-							} else {
-								var info;
-								switch (parseInt(response.code)) {
-									case 20:
-										info = '用户不存在';
-										break;
-									case 21:
-										info = '密码错误';
-										break;
-									default:
-										info = '登录错误';
-										break;
-								}
-								return $q.reject(info);
-							}
-						});
-						*/
 			loginResource.login(data)
 				.then(function(response) {
 					if (response.isSuccess) {
